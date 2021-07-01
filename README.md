a66562cf4cc7af97cc4c5690d220c31078ef7fe5
Shelby <shelbylb97@comcast.net>
2021-06-30 20:06:47 -0400

    Updating README and implementing color changing in JS

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Daily-Calender
+
+<p> I have made a calender app, running from 9 am to 9pm to match my work schedule. When you click save, what you have added to that hour will be saved in local storage.  
+
+<img width="1447" alt="Screen Shot 2021-06-30 at 8 02 06 PM" src="https://user-images.githubusercontent.com/82404980/124045654-1d271600-d9de-11eb-8b31-2ec9d4071d1d.png">
+
+<p> Links: 
+https://shelbylb97.github.io/Daily-Calender/
+https://github.com/shelbylb97/Daily-Calender
--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-console.log(moment().format("dddd, MMMM Do"));
+// declare varibles
 
-console.log(this)
-$(".saveBtn").on('click', function(){
- //get values from row being save
+//console.log(moment().format("dddd, MMMM Do"));
+$(document).ready(function () {
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    console.log(this)
+        $(".saveBtn").on('click', function(){
+    //get values from row being save
     var value = $(this).siblings(".description").val();
     var key = $(this).parent().attr("id")
 
     //store to local storage
     localStorage.setItem(key, value);
+});
 });
 
 
@@ -37,3 +43,32 @@ $("#hour-19 .description").val(localStorage.getItem("hour-19"))
 $("#hour-20 .description").val(localStorage.getItem("hour-20"))
 
 $("#hour-21 .description").val(localStorage.getItem("hour-21"))
+
+
+function hourTracker () {
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(blockHour, currentHour)
+    
+        // determines whether user has passed the time slot
+        if(blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+})
+
+}
+hourTracker ();
